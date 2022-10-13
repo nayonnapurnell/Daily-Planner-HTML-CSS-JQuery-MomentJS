74edea3be0523dd5db114350a51b1e7e5de7498c
“Nayonna <Nayonna Purnell>
2022-10-13 00:20:57 -0400

    Added the JS logic for the currentTime color changes

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-//class="container"
-
-
-// $(document).ready(function(){
-//     for(i=0; i<8; i++){
-//         $( ".container" ).append( "<li><textarea>Test</textarea></li>" );
-//     }
-// });
-
+//Current Day <p> Tag
 $("#currentDay").text(moment().format('ddd MMM Do, YYYY'));
+
+var currentTime = new Date().getHours();
+
+//If the currentTime is in the Past
+if (currentTime <= 11) {
+    document.write("Have a good morning!");
+    document.body.style.backgroundColor="yellow";
+} //If the currentTime is in the Present
+else if (currentTime === 12) {
+    document.write("Have a good evening!");
+    document.body.style.backgroundColor="orange";
+} //If the currentTime is in the Future
+else {
+    document.write("Have a good night!");
+    document.body.style.backgroundColor="grey";
+}
+
